9e2f6a16644bb8209baf7c2f0e1547407002218d
006ADD <zhakypbekov.adilet@bk.ru>
2024-03-30 22:25:36 +0600

    10.JDBC Starter

--- src/main/java/org/add/spring/database/repository/FilterUserRepository.java
+++ src/main/java/org/add/spring/database/repository/FilterUserRepository.java
@@ -2,7 +2,9 @@ package org.add.spring.database.repository;
 
 
 
+import org.add.spring.database.entity.Role;
 import org.add.spring.database.entity.User;
+import org.add.spring.dto.PersonalInfo;
 import org.add.spring.dto.UserFilter;
 
 import java.util.List;
@@ -10,4 +12,10 @@ import java.util.List;
 public interface FilterUserRepository {
 
     List<User> findAllByFilter(UserFilter filter);
+
+    List<PersonalInfo> findAllByCompanyIdAndRole(Integer companyId, Role role);
+
+    void updateCompanyAndRole(List<User> users);
+
+    void updateCompanyAndRoleNamed(List<User> users);
 }
